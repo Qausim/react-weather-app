70b1af3994082a1ce1a26e37ee848c36408307b7
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-09-19 16:01:52 +0100

    feature(weather): implement the weather details component

---
+++ src/components/Location.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/location.css';
+
+
+const Location = () => (
+  <div className="location-wrapper">
+    <p className="location">
+      Location:
+      <span> Lagos</span>
+    </p>
+  </div>
+);
+
+export default Location;
